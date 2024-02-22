8e85f6f7bae2098e7463cee97844da3a5b343074
wayne <QWE24367705@gmail.com>
2024-02-22 18:13:46 +0800

    修改 find_other_data 找尋方式 & database 程式碼整理

--- database/news.py
+++ database/news.py
@@ -4,22 +4,6 @@ import os
 client = MongoClient(os.environ.get("DB"))
 news_db = client.db
 
-def insert_mary_data(data):
-    news_db["news"].insert_many(data)
-    print('儲存成功')
-
-def delete_document():
-    news_db["news"].delete_many({})
-    print('資料已全部刪除')
-
-def find_other_data(news_name):
-    find_data = news_db["news"].find({'name':news_name})
-    data= []
-    for api in find_data:
-        api['_id'] = str(api['_id'])
-        data.append(api)
-    return list(data)
-
 def find_all_data(page):
     find_data = news_db["news"].find().skip(page * 50).limit(50)
     data= []
@@ -27,3 +11,19 @@ def find_all_data(page):
         api['_id'] = str(api['_id'])
         data.append(api)
     return list(data)
+
+def find_other_data(news_name):
+    find_data = news_db["news"].find({'en':news_name})
+    data= []
+    for api in find_data:
+        api['_id'] = str(api['_id'])
+        data.append(api)
+    return list(data)
+
+def insert_mary_data(data):
+    news_db["news"].insert_many(data)
+    print('儲存成功')
+
+def delete_document():
+    news_db["news"].delete_many({})
+    print('資料已全部刪除')
