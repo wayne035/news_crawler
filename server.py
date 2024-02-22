e6882de3c4b2986dd136498114845b7343840374
wayne <QWE24367705@gmail.com>
2024-02-22 13:32:11 +0800

    建立取得其他新聞route api

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from dotenv import load_dotenv
-from database.news import find_all_data
+from database.news import find_all_data, find_other_data
 import os
 
 load_dotenv()
@@ -12,9 +12,13 @@ app.json.ensure_ascii = False
 limiter = Limiter(
     get_remote_address,
     app=app,
-    default_limits=["1/second"],
+    default_limits=["300/day"],
 )
 CORS(app, resources={r'/.*': {'origins':f'{os.environ.get("URL")}'}})
+
+@app.route('/news/<news_name>')
+def other_news(news_name):
+    return find_other_data(news_name)
 
 @app.route('/news/<int:page>')
 def news(page):
